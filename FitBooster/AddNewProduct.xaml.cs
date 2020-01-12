40d13220f00588edb91b63ff758dd60db7858ec4
nat13alia <natalia13krawczyk@gmail.com>
2020-01-12 15:29:54 +0100

    Adding AddNewProduct Window

---
+++ FitBooster/AddNewProduct.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FitBooster
+{
+    /// <summary>
+    /// Interaction logic for AddNewProduct.xaml
+    /// </summary>
+    public partial class AddNewProduct : Window
+    {
+        public AddNewProduct()
+        {
+            InitializeComponent();
+        }
+    }
+}
