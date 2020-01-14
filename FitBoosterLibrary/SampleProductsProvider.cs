aa40512211aeda8b8d48ebe7b33c3b9095747bc9
unknown <adrian.kurek93@gmail.com>
2020-01-14 18:11:05 +0100

    Add SampleProductsProvider

---
+++ FitBoosterLibrary/SampleProductsProvider.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace FitBoosterLibrary
+{
+	// Class that provides sample products for the application.
+	public class SampleProductsProvider : IProductsProvider
+	{
+		private static List<Product> _products = new List<Product>()
+		{
+			{ new Product("Cottage cheese", "Fresh and tasty", MeasurementUnits.Grams, 200, 194, 10, 4, 22) },
+			{ new Product("Apple", "Fresh and sweet fruit", MeasurementUnits.Grams, 180, 90, 0.7, 18.2, 3.6) },
+			{ new Product("Egg", "Ingredient of many dishes", MeasurementUnits.Grams, 56, 78, 5.4, 0.3, 7) }
+		};
+
+		public void AddProduct(Product product) => _products.Add(product);
+		public void RemoveProduct(Product product) => _products.Remove(product);
+		public List<Product> GetAllProducts() => _products;
+	}
+}
