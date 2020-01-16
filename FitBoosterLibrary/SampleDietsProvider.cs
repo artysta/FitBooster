b2ad11304ece9f341bb1bf43c50d806afdb315fb
unknown <adrian.kurek93@gmail.com>
2020-01-16 22:25:46 +0100

    Add SampleDietsProvider

---
+++ FitBoosterLibrary/SampleDietsProvider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace FitBoosterLibrary
+{
+	// Class that provides sample products for the application.
+	public class SampleDietsProvider : IDietsProvider
+	{
+		private static List<Diet> _diets = new List<Diet>();
+
+		public void AddDiet(Diet diet) => _diets.Add(diet);
+		public void RemoveDiet(Diet diet) => _diets.Remove(diet);
+		public List<Diet> GetAllDiets() => _diets;
+	}
+}
